ce233eb392b5e5cd02b49cd0ff0fb9e1b53c02e0
sunjoon lee <sun8404@gmail.com>
2020-09-14 08:42:03 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TodoHead from './Components/TodoHead';
 import Counter from './Countter';
 import InputSample from './InputSample';
 import ServiceSlot from './Components/ServiceSlot';
+import TestCode from'./Components/Test';
 
 
 
@@ -30,9 +31,10 @@ class App extends Component{
         <br/><br/><br/><br/><br/>
         <ServiceSlot title = "Wacha" status = "Now On" link = "https://nexflix.com"/>
         <ServiceSlot title = "Apple Music" status = "Now On" link = "https://nexflix.com"/>
+        <TestCode name="aaa"/>
       </TodoTemplate>
 
-
+      
       <Counter />
       <br></br>
       <InputSample />
---
+++ src/Components/Test.js
@@ -0,0 +1,13 @@
+import React from'react';
+
+function TestCode({name}){
+    return(
+        <div>
+            <h1>
+                I like {name}
+            </h1>;
+        </div>
+    );
+}
+
+export default TestCode;
